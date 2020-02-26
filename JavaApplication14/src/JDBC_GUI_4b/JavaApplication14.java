2967e99b333e019b4b61b9d2631b5c4eec6e6518
sinkas692 <sinkas15@htl-shkoder.com>
2020-02-26 08:43:02 +0100

    Netbeans Projekt erstellt

---
+++ JavaApplication14/src/JDBC_GUI_4b/JavaApplication14.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC_GUI_4b;
+
+/**
+ *
+ * @author sinkas15
+ */
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
